6e58d86a528e19b8b74a622a7f016816216daec7
Cmatrix1 <sadeghmajidi980@gmail.com>
2024-02-09 13:44:31 +0330

    add: headers and cookies for requests

---
+++ constance.py
@@ -0,0 +1,21 @@
+cookies = {
+    'gridcookie': 'grid',
+    'lepopup-onload-gXfYHErM9863FAh6': '1681307909',
+    'PHPSESSID': '43e46e1b901b578fd6293e8e8c54e9b4',
+    'simplebannerclosed': 'true',
+}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://test-drive.ir/^%^D8^%^A2^%^D8^%^B2^%^D9^%^85^%^D9^%^88^%^D9^%^86-^%^D9^%^87^%^D8^%^A7^%^DB^%^8C-^%^D8^%^A7^%^D8^%^B5^%^D9^%^84^%^DB^%^8C-^%^D8^%^A2^%^DB^%^8C^%^DB^%^8C^%^D9^%^86-^%^D9^%^86^%^D8^%^A7^%^D9^%^85^%^D9^%^87-2/',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'gridcookie=grid; lepopup-onload-gXfYHErM9863FAh6=1681307909; PHPSESSID=43e46e1b901b578fd6293e8e8c54e9b4; simplebannerclosed=true',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+}
